cbf3766a43c9dbea7c72c63a37da3bfb2b28c7b2
katcozadd <krosecozadd@gmail.com>
2018-08-13 12:26:59 -0600

    while and for loop review

--- index.js
+++ index.js
@@ -3,26 +3,43 @@
 
 // Using the provided variable definitions, replace the blanks with a mathematical or boolean operator that evaluates the expression to true.
 // Test your answers by using console.log in front of each expression in your answer file.
-  const a = 4;
-  const b = 53;
-  const c = 57;
-  const d = 16;
-  const e = 'Kevin';
+//   const a = 4;
+//   const b = 53;
+//   const c = 57;
+//   const d = 16;
+//   const e = 'Kevin';
 
 
-// 1.)
-console.log(a != b);
-// 2.)
-console.log(c > d);
-// 3.)
-console.log('Name' === 'Name');
-// 4.)
-console.log(a + b === c);
-// 5.)
-console.log(a * a === d);
-// 6.)
-console.log(e === 'Kevin');
-// 7.)
-console.log(48 == '48');
+// // 1.)
+// console.log(a != b);
+// // 2.)
+// console.log(c > d);
+// // 3.)
+// console.log('Name' === 'Name');
+// // 4.)
+// console.log(a + b === c);
+// // 5.)
+// console.log(a * a === d);
+// // 6.)
+// console.log(e === 'Kevin');
+// // 7.)
+// console.log(48 == '48');
 
 
+//write a while loop that prints out the letter A 21 times
+// let letters = "A";
+// let i = 0;
+
+// while (i < 20) {
+// 	letters += "A";
+// 	i++;
+// }
+
+// console.log(letters);
+
+
+// Write a for loop that will console.log the numbers 0 to 999.
+
+for (let i = 0; i <= 999; i++) {
+	console.log(i);
+}
