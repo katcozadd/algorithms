cf62afe1997248ae4ab17afbe0b6d2848ac60728
katcozadd <krosecozadd@gmail.com>
2018-08-13 12:31:09 -0600

    decrementing for loop

--- index.js
+++ index.js
@@ -9,7 +9,6 @@
 //   const d = 16;
 //   const e = 'Kevin';
 
-
 // // 1.)
 // console.log(a != b);
 // // 2.)
@@ -26,7 +25,8 @@
 // console.log(48 == '48');
 
 
-//write a while loop that prints out the letter A 21 times
+
+// Write a while loop that prints out the letter A 21 times
 // let letters = "A";
 // let i = 0;
 
@@ -38,8 +38,34 @@
 // console.log(letters);
 
 
+
 // Write a for loop that will console.log the numbers 0 to 999.
 
-for (let i = 0; i <= 999; i++) {
+// for (let i = 0; i <= 999; i++) {
+// 	console.log(i);
+// }
+
+
+
+// For loop control panel
+// Here is an example for loop that prints a message 100 times:
+
+// for (let i=0; i < 100; i++) {
+// 	console.log('Without you, today\'s emotions are the scurf of yesterday\'s');
+// }
+// What are the three components of the control panel? Each component is separated by a semicolon ;.
+
+// Write your answers as comments in the file.
+
+// The first part of the control panel is: initialization
+// The second part of the control panel is: condition
+// The third part of the control panel is: increment or decrement
+
+
+
+// For loop in reverse
+// Using a postfix operator i-- instead of i++, write a for loop that iterates in reverse. Console.log a countdown from 999 to 0.
+
+for (let i = 999; i >= 0; i--) {
 	console.log(i);
 }
