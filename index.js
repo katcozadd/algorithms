8b2f6d9721b20759a5cfed6bc18fd3ab7788865f
katcozadd <krosecozadd@gmail.com>
2018-08-13 12:12:07 -0600

    boolean review

---
+++ index.js
@@ -0,0 +1,28 @@
+// Boolean expressions
+// ... and variable assignment
+
+// Using the provided variable definitions, replace the blanks with a mathematical or boolean operator that evaluates the expression to true.
+// Test your answers by using console.log in front of each expression in your answer file.
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+
+// 1.)
+console.log(a != b);
+// 2.)
+console.log(c > d);
+// 3.)
+console.log('Name' === 'Name');
+// 4.)
+console.log(a + b === c);
+// 5.)
+console.log(a * a === d);
+// 6.)
+console.log(e === 'Kevin');
+// 7.)
+console.log(48 == '48');
+
+
